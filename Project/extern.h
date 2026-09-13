51187c8c7d8d09f65b8a50232ac8ecfdf3e3be74
Zylohw <pvasquez2206@gmail.com>
2026-09-13 14:03:03 -0600

    update: files

---
+++ Project/extern.h
@@ -0,0 +1,5 @@
+#ifndef __EXTERN_H__
+#define __EXTERN_H__
+extern void my_strcpy(const char *src, char *dst);
+extern void my_capitalize(char *str);
+#endif /* __EXTERN_H__ */
---
+++ Project/main.c
@@ -0,0 +1,12 @@
+#include "RTE_Components.h"
+#include CMSIS_device_header
+#include "extern.h"
+static const char a[] = "Hola Mundo!";
+static char b[16];
+int main(void){
+ my_strcpy(a, b);
+ my_capitalize(b);
+    for(;;){
+
+ }
+}
