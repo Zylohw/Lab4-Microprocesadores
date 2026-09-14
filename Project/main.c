5a3731ec20f6db7439467cf00c880cf6267110f7
Zylohw <pvasquez2206@gmail.com>
2026-09-13 22:57:13 -0600

    update: main.c for test

--- Project/main.c
+++ Project/main.c
@@ -1,10 +1,16 @@
 #include "RTE_Components.h"
 #include CMSIS_device_header
 #include "extern.h"
-static const char a[] = "Hola Mundo!";
+static const char a[] = "Progamar";
+static const char z[] = "Comer";
+static const char f[] = "Casa";
 static char b[16];
+static char c[16];
+static char s[16];
 int main(void){
  my_strcpy(a, b);
+ my_strcpy(z, c);
+ my_strcpy(f, s);
  my_capitalize(b);
     for(;;){
 
